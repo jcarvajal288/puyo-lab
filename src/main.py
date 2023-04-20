07743a8915a288b3f7d83c34593812578d3b454e
Max Carvajal <jcarvajal288@gmail.com>
2023-04-20 14:57:12 -0500

    Blit red puyo sprite to screen

--- src/main.py
+++ src/main.py
@@ -3,15 +3,39 @@ import pygame
 # pygame setup
 pygame.init()
 
-background_image = pygame.image.load("../img/chalkboard_800x600.jpg")
-
 class GameState():
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
         self.isRunning = True
 
+class PuyoSprites():
+    _spritesheet_filename = "../img/chalkpuyo_sprites.png"
+    _redPuyoRect = (192, 319, 64, 64)
+
+    def __init__(self):
+        try:
+            self._spritesheet = pygame.image.load(self._spritesheet_filename).convert_alpha()
+        except pygame.error as e:
+            print(f"Unable to load spritesheet image: {self._spritesheet_filename}")
+            raise SystemExit(e)
+        self.redPuyo = self.image_at(self._redPuyoRect)
+    
+    def image_at(self, rectangle, colorkey=None):
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size, pygame.SRCALPHA)
+        image.blit(self._spritesheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey == -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+
+
 def gameLoop(gameState):
+    background_image = pygame.image.load("../img/chalkboard_800x600.jpg")
+    puyoSprites = PuyoSprites()
+
     while gameState.isRunning:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -20,10 +44,11 @@ def gameLoop(gameState):
                 gameState.isRunning = False
 
         # fill the screen with a color to wipe away anything from last frame
-        gameState.screen.fill("black")
+        gameState.screen.fill("purple")
         gameState.screen.blit(background_image, (0, 0))
 
         # RENDER YOUR GAME HERE
+        gameState.screen.blit(puyoSprites.redPuyo, (0, 0))
 
         # flip() the display to put your work on screen
         pygame.display.flip()

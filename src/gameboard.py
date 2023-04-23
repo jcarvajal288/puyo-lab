e17b3b51b300066159cb8c44e15d7f5f00cf789f
Max Carvajal <jcarvajal288@gmail.com>
2023-04-23 14:05:09 -0500

    Allow moving horizontally when in the phantom row

--- src/gameboard.py
+++ src/gameboard.py
@@ -41,7 +41,7 @@ class Gameboard:
     def is_move_legal(self, x, y, a, b):
         return min(x, a) >= 0 \
                and max(x, a) < BOARD_TILE_WIDTH \
-               and min(y, b) >= 0 \
+               and min(y, b) >= -1 \
                and max(y, b) < BOARD_TILE_HEIGHT \
                and self.board[y][x] is None \
                and self.board[b][a] is None

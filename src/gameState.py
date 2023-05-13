e847c4b28dc03569d04ee8984b160689caaf5946
Max Carvajal <jcarvajal288@gmail.com>
2023-05-13 11:56:23 -0500

    Add puyo queue under the hood

--- src/gameState.py
+++ src/gameState.py
@@ -1,3 +1,4 @@
+from collections import deque
 import event
 from puyoPair import PuyoPair
 
@@ -6,7 +7,7 @@ class GameState:
     def __init__(self, pygame):
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
-        self.puyo_pair = PuyoPair()
+        self.puyo_queue = deque([PuyoPair() for _ in range(3)])
         self.max_chain = 0
         self.num_moves = 0
         self.isRunning = True
@@ -15,25 +16,31 @@ class GameState:
         self.max_chain = 0
         self.num_moves = 0
         board.reset()
-        self.puyo_pair = PuyoPair()
+        self.puyo_queue = deque([PuyoPair() for _ in range(3)])
+
+    def current_pair(self):
+        return self.puyo_queue[0]
+
+    def next_pair(self):
+        self.puyo_queue.append(PuyoPair())
+        self.puyo_queue.popleft()
 
     def handle_event(self, evnt, board):
         if evnt == event.EVENT_QUIT:
             self.isRunning = False
         elif event.is_movement_event(evnt):
             if evnt == event.INPUT_UP:
-                self.puyo_pair.quick_drop(board)
-                board.set_puyos(self.puyo_pair.colors, self.puyo_pair.locations)
-                self.puyo_pair = PuyoPair()
+                self.current_pair().quick_drop(board)
+                board.set_puyos(self.current_pair().colors, self.current_pair().locations)
+                self.next_pair()
                 self.num_moves += 1
             else:
-                puyos_placed = self.puyo_pair.move(board, evnt)
+                puyos_placed = self.current_pair().move(board, evnt)
                 if puyos_placed:
                     self.num_moves += 1
         elif evnt == event.ROTATE_CLOCKWISE:
-            self.puyo_pair.rotate_clockwise(board)
+            self.current_pair().rotate_clockwise(board)
         elif evnt == event.ROTATE_COUNTER_CLOCKWISE:
-            self.puyo_pair.rotate_counter_clockwise(board)
+            self.current_pair().rotate_counter_clockwise(board)
         elif evnt == event.RESET_GAME:
             self.reset(board)
-
--- src/renderer.py
+++ src/renderer.py
@@ -10,7 +10,6 @@ class Renderer:
         animation_frame = self._pygame.time.get_ticks() // sprites.ANIMATION_SPEED_IN_MS % 4
         board.draw(game_state.screen, puyo_sprites, animation_frame)
         if not board.is_resolving():
-            game_state.puyo_pair.draw(game_state.screen, board, puyo_sprites, animation_frame)
+            game_state.current_pair().draw(game_state.screen, board, puyo_sprites, animation_frame)
 
-        # flip() the display to put your work on screen
         self._pygame.display.flip()
